3ca5211f4086326297408544c6a9c09256bd87eb
ben <ben.navy@gmail.com>
2014-11-30 22:27:19 -0600

    cosmetic changes

--- api/controllers/ProfileController.js
+++ api/controllers/ProfileController.js
@@ -12,10 +12,27 @@ module.exports = {
   index:function(req,res){
     var content  = {};
     content.user = req.user[0];
+    var q = {user_id:content.user.id}
+    var q = {}
+    Job.find(q).exec(function(err,jobs){
+      Actions.find({user_id:content.user.id}).exec(function(err,actions){
+        // var actions_json = _.pluck(actions, 'createdAt')
 
-    Job.find({}).exec(function(err,jobs){
-      content.jobs = err || jobs;
-      res.view( "profile/index", content );
+        actions_json = _.groupBy(actions, function(action) {
+          var d = new Date( Date.parse( action.createdAt ) );
+          return d.getDay()+"."+d.getHours()
+        });
+        var actions = []
+        console.log(actions_json)
+        for( a in actions_json){
+          var ac = {x:a, y:actions_json[a].length} 
+          actions.push( ac )
+        }
+        console.log(actions)
+
+        content.actions = actions
+        res.view( "profile/index", { jobs:jobs, actions:actions, user:req.user[0] } );
+      })
     })
 
     return;
